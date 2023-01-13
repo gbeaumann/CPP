cc57584f675526e696bf6969e43bc57f6b8ca7b9
Gregoire Beaumann <gbeauman@c1r2s1.42lausanne.ch>
2023-01-13 17:45:04 +0100

    ajout

--- module03/ex01/SkavTrap_class.cpp
+++ module03/ex01/SkavTrap_class.cpp
@@ -6,10 +6,30 @@
 /*   By: gbeauman <gbeauman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 16:47:45 by gbeauman          #+#    #+#             */
-/*   Updated: 2023/01/13 16:48:44 by gbeauman         ###   ########.fr       */
+/*   Updated: 2023/01/13 17:34:36 by gbeauman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"SkavTrap_class.hpp"
 
 SkavTrap::SkavTrap(void)
+{
+	std::cout << "SkavTrap " << this->_Name << " was created!" << std::endl;
+}
+
+~SkavTrap::SkavTrap(void)
+{
+	std::cout << "SkavTrap " << this->_Name << " was destroyed!" << std::endl;
+}
+
+SkavTrap	&SkavTrap::operator=(const SkavTrap &copy)
+{
+	if (this != &copy)
+		this = &copy)
+	return (*this);
+}
+
+SkavTrap::SkavTrap(const SkavTrap &copy)
+{
+	*this = copy;
+}

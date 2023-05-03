78afed01c5657172c2071a02440eb176791e8dd0
Grégoire Beaumann <gregoirebeaumann@MacBook-Pro-de-Gregoire.local>
2023-05-03 16:23:23 +0200

    mod08 ex00

--- module08/ex00/easyfind.hpp
+++ module08/ex00/easyfind.hpp
@@ -1,16 +1,28 @@
 #ifndef	EASYFIND_HPP
 # define EASYFIND_HPP
 
-# include	<iostream>
-# include	<algorithm>
 # include	<vector>
+# include	<algorithm>
+# include	<stdio.h>
+# include	<iostream>
 
-template	<typename	T>
-typename T::iterator	easyFind(T &myContainer, int find)
+class	NotFoundException : public std::exception
 {
+	public:
+		virtual const char	*what() const throw()
+		{
+			return ("Exception: was not found in container");
+		}
+};
 
-	return (std::find(myContainer.begin(), myContainer.end(), find));
-
+template	<typename T>		
+typename T::iterator	easyfind(T &myContainer, int toFind)
+{
+	typename T::iterator	ret = std::find(myContainer.begin(), myContainer.end(), toFind);
+	if (ret == myContainer.end())
+		throw NotFoundException();
+	else
+		return (ret);
 }
 
 #endif
